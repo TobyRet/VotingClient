860bb9988abb5eacba75a343fc34447be6c85e74
tobyret <toby.retallick@gmail.com>
2016-09-20 13:43:43 +0100

    Unit tests for voting component

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+      exclude: /node_modules/,
+      loader: 'babel-loader'
     }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: __dirname + './dist',
+    path: __dirname.join('./dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
