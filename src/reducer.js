12d2488d9346b3878b522b06cb94f21b23a616ba
tobyret <toby.retallick@gmail.com>
2016-10-04 10:47:58 +0100

    Create reducer to manage state

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,35 @@
-import React from 'react'
+import {Map, List} from 'immutable'
 
+function resetVote (state) {
+  const hasVoted = state.get('hasVoted')
+  const currentPair = state.getIn(['vote', 'pair'], List())
+  if (hasVoted && !currentPair.includes(hasVoted)) {
+    return state.remove('hasVoted')
+  } else {
+    return state
+  }
+}
+
+function setState (state, setState) {
+  return state.merge(setState)
+}
+
+function vote (state, entry) {
+  const currentPair = state.getIn(['vote', 'pair'])
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry)
+  } else {
+    return state
+  }
+}
+
+export default function (state = Map(), action) {
+  switch (action.type) {
+    case 'SET_STATE':
+      return resetVote(setState(state, action.state))
+    case 'VOTE':
+      return vote(state, action.entry)
+  }
+  return state
+}
 
